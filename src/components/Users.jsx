e1ec64f6de1c79699e5f8e5d47f1e7012f093d0d
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2022-11-12 15:17:15 +0700

    first commit

---
+++ src/components/Users.jsx
@@ -0,0 +1,44 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const Users = () => {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        const timeId = setTimeout(() => {
+            axios.get('http://localhost:3001/users')
+                .then(res => {
+                    setUsers(res.data)
+                    setLoading(false)
+                })
+
+            return () => {
+                clearTimeout(timeId)
+            }
+        }, 3000)
+    }, [])
+
+    return (
+        <div className='wrapper'>
+            <h1>Users</h1>
+            {loading ? (
+                <>
+                    <FontAwesomeIcon icon={faSpinner} className='loading'/>
+                    <p>Loading...........</p>
+                </>
+            ) : (
+                <ul>
+                    {users.map(user => (
+                        <li key={user.id}>{user.name}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+export default Users
