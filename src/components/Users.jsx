c892502ddc205301b0f9eb01bd1c15a694eda794
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2022-11-23 17:30:00 +0700

    make it better

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,42 +1,42 @@
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Users = () => {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        setLoading(true)
-        const timeId = setTimeout( async() => {
-            const data = await axios.get('http://localhost:3001/users')
-            setUsers(data.data)
-            setLoading(false)
+        setLoading(true);
+        const timeId = setTimeout(async () => {
+            const data = await axios.get("http://localhost:3001/users");
+            setUsers(data.data);
+            setLoading(false);
 
             return () => {
-                clearTimeout(timeId)
-            }
-        }, 3000)
-    }, [])
+                clearTimeout(timeId);
+            };
+        }, 3000);
+    }, []);
 
     return (
-        <div className='wrapper'>
+        <div className="wrapper">
             <h1>Users</h1>
             {loading ? (
                 <>
-                    <FontAwesomeIcon icon={faSpinner} className='loading'/>
+                    <FontAwesomeIcon icon={faSpinner} className="loading" />
                     <p>Loading...........</p>
                 </>
             ) : (
                 <ul>
-                    {users.map(user => (
+                    {users.map((user) => (
                         <li key={user.id}>{user.name}</li>
                     ))}
                 </ul>
             )}
         </div>
-    )
-}
+    );
+};
 
-export default Users
+export default Users;
